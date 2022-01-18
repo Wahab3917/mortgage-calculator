f8d50d1f0c3bf6b1510e8f32b7189f711349f1d1
Abdul Wahab <wahab3917@gmail.com>
2022-01-18 20:44:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// Listen For Submit
+document.getElementById("loan-form").addEventListener("submit", computeResults);
+
+function computeResults(e) {
+
+  // UI
+  const UIamount = document.getElementById("amount").value;
+  const UIinterest = document.getElementById("interest").value;
+  const UIyears = document.getElementById("years").value;
+
+  // Calculate
+  const principal = parseFloat(UIamount);
+  const CalculateInterest = parseFloat(UIinterest) / 100 / 12;
+  const calculatedPayments = parseFloat(UIyears) * 12;
+
+  //Compute monthly Payment
+  const x = Math.pow(1 + CalculateInterest, calculatedPayments);
+  const monthly = (principal * x * CalculateInterest) / (x - 1);
+  const monthlyPayment = monthly.toFixed(2);
+
+  //Compute Interest
+  const totalInterest = (monthly * calculatedPayments - principal).toFixed(2);
+
+  //Compute Total Payment
+  const totalPayment = (monthly * calculatedPayments).toFixed(2);
+
+  //Show results
+  document.getElementById("monthlyPayment").innerHTML = "$" + monthlyPayment + "  Monthly Payments";
+  document.getElementById("totalInterest").innerHTML = "%" + totalInterest + "  Total Interest";
+  document.getElementById("totalPayment").innerHTML = "$" + totalPayment + "  Total Payment";
+
+  e.preventDefault();
+
+}
+
